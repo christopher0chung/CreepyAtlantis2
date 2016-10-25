18ec0f346e8773bc7554bf5d5dfd79734361da3c
Chris Chung <christopher0chung@gmail.com>
2016-10-25 13:21:48 -0400

    Demo

---
+++ SFAssets/Assets/LightBeamBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBeamBehavior : MonoBehaviour {
+
+    private float timer;
+    private SpriteRenderer mySR;
+
+	// Use this for initialization
+	void Start () {
+
+        timer = Random.Range(-6000, -5000);
+        mySR = GetComponent<SpriteRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+        mySR.color = new Color(mySR.color.r, mySR.color.g, mySR.color.b, .05f * Mathf.Sin(timer));
+	}
+}
