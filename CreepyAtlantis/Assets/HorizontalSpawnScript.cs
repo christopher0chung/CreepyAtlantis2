5d466fc82a7fb2ba5c197be4e1c01e9f023c0a81
Chris Chung <christopher0chung@gmail.com>
2016-10-25 15:23:19 -0400

    Camera and ground.

---
+++ CreepyAtlantis/Assets/HorizontalSpawnScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalSpawnScript : MonoBehaviour {
+
+    public GameObject ground1;
+
+    public int leftMax;
+    public int RightMax;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = leftMax; i < RightMax; i++)
+        {
+            Instantiate(ground1, new Vector3(i, 0, 0), Quaternion.identity, transform);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
