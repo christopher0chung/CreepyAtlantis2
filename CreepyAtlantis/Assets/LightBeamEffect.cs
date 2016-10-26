daf1b7d87c7a06d3af272f9e83ca0b024c6bc876
Chris Chung <christopher0chung@gmail.com>
2016-10-26 11:03:26 -0400

    Player and lights.

---
+++ CreepyAtlantis/Assets/LightBeamEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBeamEffect : MonoBehaviour {
+
+    private SpriteRenderer mySR;
+    private float timer;
+    private float periodScalar;
+
+	// Use this for initialization
+	void Start () {
+        mySR = GetComponent<SpriteRenderer>();
+        timer = Random.Range(-2000, -1000);
+        periodScalar = Random.Range(.2f, .6f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        mySR.color = new Color(mySR.color.r, mySR.color.g, mySR.color.b, Mathf.Sin(timer * periodScalar) * .025f);
+	}
+}
---
+++ CreepyAtlantis/Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody2D myRB;
+
+    public float thrustForce;
+    public float walkForce;
+    public float onGroundScale;
+    public float notOnGroundScale;
+
+    private float scale;
+
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody2D>();
+        scale = notOnGroundScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRB.AddForce(Vector2.right * walkForce * scale);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRB.AddForce(Vector2.right * -walkForce * scale);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRB.AddForce(Vector2.up * thrustForce);
+        }
+
+    }
+
+    void OnCollisionStay2D ()
+    {
+        scale = onGroundScale;
+    }
+
+    void OnCollisionExit2D ()
+    {
+        scale = notOnGroundScale;
+    }
+}
