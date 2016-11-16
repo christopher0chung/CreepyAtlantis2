14e0142b5583f23f6d267b44c3e8d024f03edf71
Chris Chung <christopher0chung@gmail.com>
2016-11-16 09:34:48 -0500

    Event state

---
+++ CreepyAtlantis/Assets/GameStateManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateManager : MonoBehaviour {
+
+    public Controllables[] controlStatus = new Controllables[2];
+
+    public delegate void SwitchControls(int player, Controllables target);
+    public static event SwitchControls onSwap;
+
+    public void Swap (int player, Controllables target)
+    {
+        onSwap(player, target);
+        controlStatus[player] = target;
+    }
+
+    public delegate void IngressEgress(int player, bool ingress);
+    public static event IngressEgress onSubInteract;
+
+    public void SubInteract (int player, bool ingress)
+    {
+        onSubInteract(player, ingress);
+    }
+}
+
+public enum Controllables { character1, character2, submarine }
