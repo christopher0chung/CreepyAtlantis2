d4c6447a48362c9cb27d17bc6eb84cfb56c47897
Chris Chung <christopher0chung@gmail.com>
2017-02-07 11:49:50 -0500

    Modified player light controls.

--- CreepyAtlantis/Assets/Scripts/PlayerLightAngle.cs
+++ CreepyAtlantis/Assets/Scripts/PlayerLightAngle.cs
@@ -13,7 +13,8 @@ public class PlayerLightAngle : MonoBehaviour {
 	void Update () {
 
         transform.position = transform.root.position + Vector3.up * 1.373f;
-        transform.rotation = Quaternion.Euler(0, 0, lightAngle);
+        lightAngle = Mathf.Clamp (lightAngle, -60, 60);
+        transform.localRotation = Quaternion.Euler(lightAngle, 0, 0);
 
 
     }
