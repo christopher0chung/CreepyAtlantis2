6350eafed4424641881ab590f6f1bab8b6600121
Chris Chung <christopher0chung@gmail.com>
2017-02-16 17:22:48 -0500

    Air bar attempt merge.

---
+++ CreepyAtlantis/Assets/AirMeter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirMeter : MonoBehaviour {
+
+    [SerializeField] private float airPercent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
