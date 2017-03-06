e6d58395f4acd9d1257a9593f4dfba15e29b37be
Chris Chung <christopher0chung@gmail.com>
2017-03-06 18:40:04 -0500

    Buoyancy script and physics based movement started.

--- CreepyAtlantis/Assets/Scripts/BuoyancyScript.cs
+++ CreepyAtlantis/Assets/Scripts/BuoyancyScript.cs
@@ -3,21 +3,35 @@ using System.Collections;
 
 public class BuoyancyScript : MonoBehaviour {
 
-    public float period;
-    public float magnitude;
+    //public float period;
+    //public float magnitude;
     private float originalHeight;
-    private float timer;
+    //private float timer;
+
+    private Rigidbody myRB;
+    private bool upDown;
 
 	// Use this for initialization
 	void Start () {
         originalHeight = transform.position.y;
+        myRB = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	//// Update is called once per frame
+	//void Update () {
 
-        timer += Time.deltaTime;
-        transform.position = new Vector3(transform.position.x, originalHeight + magnitude * Mathf.Sin(timer * period), transform.position.z);
+ //       timer += Time.deltaTime;
+ //       myRB.MovePosition(new Vector3(transform.position.x, originalHeight + magnitude * Mathf.Sin(timer * period), transform.position.z));
 
-	}
+	//}
+
+    void FixedUpdate()
+    {
+        myRB.AddForce(BuoyancyVector());
+    }
+
+    private Vector3 BuoyancyVector ()
+    {
+        return Vector3.zero;
+    }
 }
