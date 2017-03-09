d43504b54099dacbae990a7760533d069a8b4859
Chris Chung <christopher0chung@gmail.com>
2017-03-09 12:53:25 -0500

    Camera jitter fixed by removing lerp.

--- CreepyAtlantis/Assets/Scripts/Camera and Camera Related/CameraPositionScript.cs
+++ CreepyAtlantis/Assets/Scripts/Camera and Camera Related/CameraPositionScript.cs
@@ -11,6 +11,58 @@ public class CameraPositionScript : MonoBehaviour {
 
     void Update()
     {
+        //float lowestY = p1.position.y;
+        //if (p2.position.y < lowestY)
+        //    lowestY = p2.position.y;
+        //lowestY -= 2;
+        //lowestY = Mathf.Clamp(lowestY, 0, 15);
+
+        //float maxX = Mathf.Abs(p1.position.x - p2.position.x);
+        //if (Mathf.Abs(p1.position.x - sub.position.x) > maxX)
+        //    maxX = Mathf.Abs(p1.position.x - sub.position.x);
+        //if (Mathf.Abs(p2.position.x - sub.position.x) > maxX)
+        //    maxX = Mathf.Abs(p2.position.x - sub.position.x);
+        //maxX = Mathf.Clamp(maxX, 0, 40);
+
+        //float maxXScalar = maxX / 40;
+
+        //float maxY = Mathf.Abs(p1.position.y - p2.position.y);
+        //if (Mathf.Abs(p1.position.y - sub.position.y) > maxY)
+        //    maxY = Mathf.Abs(p1.position.y - sub.position.y);
+        //if (Mathf.Abs(p2.position.y - sub.position.y) > maxY)
+        //    maxY = Mathf.Abs(p2.position.y - sub.position.y);
+        //maxY = Mathf.Clamp(maxY, 0, 20);
+
+        //float maxYScalar = maxY / 20;
+
+        ////Debug.Log(maxXScalar + " " + maxYScalar);
+
+        //float maxScalar = maxXScalar;
+        //if (maxYScalar > maxScalar)
+        //    maxScalar = maxYScalar;
+
+        //float depth = Mathf.Lerp(-25, -45, maxScalar);
+
+        //camDest = new Vector3((p1.position.x + p2.position.x) / 2, lowestY, depth);
+        //if (camDest.x >= sub.position.x + 5)
+        //    camDest.x = sub.position.x + 5;
+        //if (camDest.x <= sub.position.x - 5)
+        //    camDest.x = sub.position.x - 5;
+
+    }
+
+    void Start()
+    {
+        Camera.onPreCull += CameraUpdate;
+    }
+
+    void OnDestroy()
+    {
+        Camera.onPreCull -= CameraUpdate;
+    }
+
+    void CameraUpdate(Camera camera) {
+
         float lowestY = p1.position.y;
         if (p2.position.y < lowestY)
             lowestY = p2.position.y;
@@ -49,11 +101,10 @@ public class CameraPositionScript : MonoBehaviour {
         if (camDest.x <= sub.position.x - 5)
             camDest.x = sub.position.x - 5;
 
-    }
+        //Vector3 prev = transform.position;
+        //transform.position = Vector3.Lerp(transform.position, camDest, 4 * Time.deltaTime);
+        //Debug.Log(Vector3.Distance(prev, transform.position));
 
-    void LateUpdate () {
-
-        transform.position = Vector3.Lerp(transform.position, camDest, 4 * Time.deltaTime);
-
+        transform.position = camDest;
     }
 }
