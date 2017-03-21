481b5656bb9937134e989f369f0aea9837d56e01
Chris Chung <christopher0chung@gmail.com>
2017-03-20 20:55:03 -0400

    Air prototype

---
+++ CreepyAtlantis/Assets/MiniGameTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameTest : MonoBehaviour {
+
+    [HideInInspector] public Vector3 ctrlInput;
+    private Vector3 gameInput;
+
+    private float timer;
+    [SerializeField] private float speed;
+    [SerializeField] private float magnitude;
+    private float timeInterval;
+
+	// Use this for initialization
+	void Start () {
+        timeInterval = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= timeInterval)
+        {
+            timer -= timeInterval;
+            timeInterval = Random.Range(.5f, 1f);
+            gameInput = new Vector3(Random.Range(-1.0f, 1.0f) * speed, Random.Range(-1.0f, 1.0f) * speed, 0);
+        }
+
+        Vector3 hold = transform.position + (ctrlInput + gameInput) * Time.deltaTime;
+        if (Vector3.Distance(hold, Vector3.zero) > magnitude)
+        {
+            hold = magnitude * Vector3.Normalize(hold);
+        }
+        transform.position = hold;
+	}
+}
---
+++ CreepyAtlantis/Assets/MiniGameTestTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameTestTrigger : MonoBehaviour {
+
+    private GameObject target;
+    private GameObject reticle;
+
+    [SerializeField] private float completion;
+    [SerializeField] private float dist;
+
+    void Start () {
+        target = GameObject.Find("Target");
+        reticle = GameObject.Find("Reticle");
+    }
+	
+	void Update () {
+        if (Vector3.Distance(target.transform.position, reticle.transform.position) < dist)
+        {
+            completion += (dist - Vector3.Distance(target.transform.position, reticle.transform.position)) * .25f * Time.deltaTime;
+            target.GetComponent<Image>().fillAmount = completion;
+        }
+
+        if (completion >= 1)
+        {
+            target.GetComponent<Image>().color = Color.red;
+        }
+	}
+}
