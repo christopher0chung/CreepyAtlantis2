5e772ccc061dec2b7618e9ec0538531beebd4f1d
Chris Chung <christopher0chung@gmail.com>
2017-04-22 20:08:04 -0400

    UI test and beginning of new objective system.

---
+++ CreepyAtlantis/Assets/GameObjective.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectiveCompletedEvent : GameEvent
+{
+
+}
+
+public class GameObjective {
+
+    public string iD;
+    public bool completed;
+
+    public GameObjective (string id)
+    {
+        iD = id;
+    }
+
+    public void Complete()
+    {
+        completed = true;
+        //EventManager.instance.Fire();
+    }
+}
---
+++ CreepyAtlantis/Assets/RingRotationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingRotationScript : MonoBehaviour {
+
+    public Transform[] elements = new Transform[5];
+
+    private float _timer;
+    private float _tickTime;
+    private Vector3[] originalRots = new Vector3[5];
+    private Vector3 rotMod;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < elements.Length; i++)
+        {
+            originalRots[i] = elements[i].localRotation.eulerAngles;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _timer += Time.deltaTime;
+        if (_timer >= _tickTime)
+        {
+            _tickTime = Random.Range(.5f, 1.1f);
+            _timer = 0;
+            rotMod = new Vector3(0, 0, Random.Range(-180, 180));
+        }
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            elements[i].localRotation = Quaternion.Slerp(elements[i].localRotation, Quaternion.Euler(originalRots[i] +rotMod), Time.deltaTime * 2);
+        }
+	}
+}
+
