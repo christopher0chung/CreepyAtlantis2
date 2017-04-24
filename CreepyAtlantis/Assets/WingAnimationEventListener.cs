22cab720739b911c2a18b3be40c3f14cea25b47f
Chris Chung <christopher0chung@gmail.com>
2017-04-24 18:15:03 -0400

    Suit animations started.

---
+++ CreepyAtlantis/Assets/WingAnimationEventListener.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WingAnimationEventListener : MonoBehaviour {
+
+    public enum WingSide { Port, Stbd }
+    [SerializeField] private WingSide WingSelector;
+
+    public enum WingStates { Retracted, Deployed, Operating, PrepForStow }
+
+    private Vector3 wingRetracted;
+    private Vector3 wingDeployed;
+    private Vector3 wingDeployed_Offset;
+
+    private FSM<WingAnimationEventListener> _fsm;
+
+    private float timer;
+    [SerializeField] private float wingTwitchTime;
+
+	// Use this for initialization
+	void Start () {
+        _fsm = new FSM<WingAnimationEventListener>(this);
+        _fsm.TransitionTo<Operate>();
+
+        if (WingSelector == WingSide.Port)
+        {
+            wingRetracted = new Vector3(0, 0, 90);
+        }
+        else
+        {
+            wingRetracted = new Vector3(0, 0, -90);
+        }
+        wingDeployed = new Vector3(-90, 0, 0);
+
+        EventManager.instance.Register<Character_Grounded_GE>(DriveState);
+	}
+
+    void OnEnable()
+    {
+        _fsm.TransitionTo<Operate>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timer += Time.deltaTime;
+        if (timer >= wingTwitchTime)
+        {
+            timer = 0;
+            wingDeployed_Offset = new Vector3(
+                Random.Range(-135, -45),
+                Random.Range(-30, 30),
+                Random.Range(-30, 30));
+        }
+
+        _fsm.Update();
+    }
+
+    void DriveState(GameEvent e)
+    {
+        if (e.GetType() == typeof(Character_Grounded_GE))
+        {
+            Character_Grounded_GE g_GE = (Character_Grounded_GE)e;
+            if (g_GE.Name == gameObject.name)
+            {
+                if (g_GE.G == GroundStates.Grounded)
+                {
+                    _fsm.TransitionTo<PrepForStow>();
+                }
+                else
+                {
+                    _fsm.TransitionTo<Deploy>();
+                }
+            }
+        }
+    }
+
+    #region Finite State Machine States
+    //---------------------------------------------------
+    // FSM States
+    //---------------------------------------------------
+
+    private class BasicState : FSM<WingAnimationEventListener>.State
+    {
+        public string name;
+    }
+
+    private class Standby : BasicState
+    {
+        public override void Init()
+        {
+            name = "Standby";
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("In " + name);
+            
+        }
+
+        public override void Update()
+        {
+            return;
+        }
+
+        public override void OnExit()
+        {
+
+        }
+    }
+
+    private class Retract : BasicState
+    {
+        public override void Init()
+        {
+            name = "Retract";
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("In " + name);
+
+        }
+
+        public override void Update()
+        {
+            Context.transform.rotation = Quaternion.Slerp(Context.transform.rotation, Quaternion.Euler(Context.wingRetracted), .07f);
+            if (Quaternion.Angle(Context.transform.rotation, Quaternion.Euler(Context.wingRetracted)) <= 1)
+                TransitionTo<Standby>();
+        }
+
+        public override void OnExit()
+        {
+            Context.transform.rotation = Quaternion.Euler(Context.wingRetracted);
+        }
+    }
+
+    private class Deploy : BasicState
+    {
+        public override void Init()
+        {
+            name = "Deploy";
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("In " + name);
+
+        }
+
+        public override void Update()
+        {
+            Context.transform.rotation = Quaternion.Slerp(Context.transform.rotation, Quaternion.Euler(Context.wingDeployed), .07f);
+            if (Quaternion.Angle(Context.transform.rotation, Quaternion.Euler(Context.wingDeployed)) <= 1)
+                TransitionTo<Operate>();
+        }
+
+        public override void OnExit()
+        {
+            Context.transform.rotation = Quaternion.Euler(Context.wingDeployed);
+        }
+    }
+
+    private class Operate : BasicState
+    {
+        public override void Init()
+        {
+            name = "Operate";
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("In " + name);
+
+        }
+
+        public override void Update()
+        {
+            Context.transform.rotation = Quaternion.Slerp(Context.transform.rotation, Quaternion.Euler(Context.wingDeployed + Context.wingDeployed_Offset), .07f);
+        }
+
+        public override void OnExit()
+        {
+
+        }
+    }
+
+    private class PrepForStow : BasicState
+    {
+        public override void Init()
+        {
+            name = "PrepForStow";
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("In " + name);
+
+        }
+
+        public override void Update()
+        {
+            Context.transform.rotation = Quaternion.Slerp(Context.transform.rotation, Quaternion.Euler(Context.wingDeployed), .07f);
+            if (Quaternion.Angle(Context.transform.rotation, Quaternion.Euler(Context.wingDeployed)) <= 1)
+                TransitionTo<Retract>();
+        }
+
+        public override void OnExit()
+        {
+            Context.transform.rotation = Quaternion.Euler(Context.wingDeployed);
+        }
+    }
+    #endregion
+}
